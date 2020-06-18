16f42fbce3b89a8b7f9ab2054c98faa98bb806ce
Teagan Keith <teagankeith@outlook.com>
2020-06-18 15:00:39 -0400

    Based Portfolio Website

---
+++ portfolio/src/App.js
@@ -0,0 +1,150 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+			<div id="wrapper" class="fade-in">
+
+					<div id="intro">
+						<h1>Teagan Keith</h1>
+						<p>A showcase of my work and abilities.</p>
+						<ul class="actions">
+							<li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+						</ul>
+					</div>
+
+
+					<header id="header">
+						<a href="index.html" class="logo">Massively</a>
+					</header>
+
+
+					<nav id="nav">
+						<ul class="links">
+							<li class="active"><a href="index.html">My Work</a></li>
+
+						</ul>
+						<ul class="icons">
+							<li><a href="https://twitter.com/teagankeith" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="https://www.linkedin.com/in/teagankeith/" class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
+							<li><a href="https://github.com/teagankeith" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+						</ul>
+					</nav>
+
+
+					<div id="main">
+
+						
+							<article class="post featured">
+								<header class="major">
+									<h2><a href="#">My name is Teagan Keith</a></h2>
+									<p>Before Lambda I had worked as a self-taught programmer, although, in my situation that seemed to not be working out. I am incredibly grateful to Lambda for giving me the structure I needed to learn programming and surrounding me with like minded people to support me as well. After Lambda I would like to work somewhere who's working on open-source applications or materials for people who were just like me before Lambda, learning how to code by their selves, and make it just a tiny bit easier.
+</p>
+								</header>
+								<a href="#" class="image main"><img src="images/pic01.jpg" alt="" /></a>
+							</article>
+
+							<section class="posts">
+								<article>
+									<header>
+										<h2><a href="#">Sed magna<br />
+										ipsum faucibus</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/pic02.jpg" alt="" /></a>
+									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">See The Code</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<h2><a href="#">Primis eget<br />
+										imperdiet lorem</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/pic03.jpg" alt="" /></a>
+									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">See The Code</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+							
+										<h2><a href="#">Ante mattis<br />
+										interdum dolor</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/pic04.jpg" alt="" /></a>
+									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">See The Code</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+							
+										<h2><a href="#">Tempus sed<br />
+										nulla imperdiet</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/pic05.jpg" alt="" /></a>
+									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">See The Code</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+							
+										<h2><a href="#">Odio magna<br />
+										sed consectetur</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/pic06.jpg" alt="" /></a>
+									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">See The Code</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<h2><a href="#">Augue lorem<br />
+										primis vestibulum</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
+									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">See The Code</a></li>
+									</ul>
+								</article>
+							</section>
+
+			
+						
+					</div>
+
+	
+					<footer id="footer">
+						<section>
+            <section>
+								<h3>Social</h3>
+								<ul class="icons alt">
+                <li><a href="https://twitter.com/teagankeith" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="https://www.linkedin.com/in/teagankeith/" class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
+							<li><a href="https://github.com/teagankeith" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+								</ul>
+							</section>
+						</section>
+					</footer>
+
+
+					<div id="copyright">
+						<ul><li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
+					</div>
+
+			</div>
+    </div>
+  );
+}
+
+export default App;
